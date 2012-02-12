95a7faaa03ac564f1d3ba9a1a307b8fe21236cd5
unknown <telesup@PC38.(none)>
2012-02-11 21:12:55 -0500

    cambio 4

---
+++ proyecto002/src/TELESUP/JAVA/PRINCIPAL/mobil2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TELESUP.JAVA.PRINCIPAL;
+
+import javax.microedition.midlet.*;
+
+/**
+ * @author telesup
+ */
+public class mobil2 extends MIDlet {
+
+    public void startApp() {
+    }
+    
+    public void pauseApp() {
+    }
+    
+    public void destroyApp(boolean unconditional) {
+    }
+}
